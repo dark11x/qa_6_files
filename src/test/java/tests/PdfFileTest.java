00a2a92c8825520224c90f6eb287cb5ea2173c12
dark11x <konets.sveta.2013@mail.ru>
2022-06-29 18:02:46 +0300

    добавлен тест на .pdf файл

---
+++ src/test/java/tests/PdfFileTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.pdftest.PDF.containsText;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PdfFileTest {
+
+    @Test
+    void pdfTest() throws IOException {
+        String pdfFilePath = "src/test/resources/files/1.pdf";
+        String expectedData = "123456";
+
+        PDF pdf = new PDF(new File(pdfFilePath));
+        assertThat(pdf, containsText(expectedData));
+    }
+}
